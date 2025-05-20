7f6a7e363844495132fd8e52a65c27f7f1068faf
rednokay <207755534+rednokay@users.noreply.github.com>
2025-05-20 12:41:54 +0200

    Add functionality to write feed to a file

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub fn read_json(path_to_json: &str) -> anyhow::Result<String> {
 pub mod channel {
     use reqwest;
     use serde::{Deserialize, Serialize};
+    use std::fs;
 
     use crate::DATA_DIR;
 
@@ -27,7 +28,7 @@ pub mod channel {
         name: String,
 
         #[serde(skip)]
-        feed_path: String,
+        pub feed_path: String,
         #[serde(skip)]
         channel_id: String,
         #[serde(skip)]
@@ -47,8 +48,12 @@ pub mod channel {
             }
         }
 
-        fn set_feed_path(&mut self) {
+        pub fn set_feed_path(&mut self) -> anyhow::Result<()> {
+            if self.channel_id.is_empty() {
+                anyhow::bail!("Channel id must be set before setting a feed path");
+            }
             self.feed_path = DATA_DIR.to_owned() + &self.channel_id;
+            Ok(())
         }
 
         pub fn parse_id(&self) -> anyhow::Result<String> {
@@ -91,14 +96,26 @@ pub mod channel {
             Ok(())
         }
 
-        pub fn write_feed_to_file() {
-            todo!();
+        pub fn write_feed_to_file(&self) -> anyhow::Result<()> {
+            if self.feed_path.is_empty() {
+                anyhow::bail!("Feed path not set!")
+            }
+            if let Some(f) = &self.feed {
+                fs::write(&self.feed_path, f)?;
+                Ok(())
+            } else {
+                anyhow::bail!("Feed not set, cannot write to file")
+            }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
+    use crate::DATA_DIR;
+
     use super::channel;
 
     #[test]
@@ -124,6 +141,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn set_feed() {
         // Torsten Heinrich
         let url = "https://www.youtube.com/channel/UC9kZ6FlOQfusBV8LS2x2fAA/";
@@ -135,5 +153,50 @@ mod tests {
         let _ = c.set_feed();
 
         assert_ne!(None, c.feed);
+    }
+
+    #[test]
+    fn set_feed_path() {
+        let url = "https://www.youtube.com/channel/UC9kZ6FlOQfusBV8LS2x2fAA/";
+        let name = "Torsten Heinrich";
+        let mut c = channel::Channel::new(0, url.into(), name.into());
+
+        assert!(c.feed_path.is_empty());
+
+        let e = c.set_feed_path();
+
+        assert!(e.is_err());
+
+        let i = c.set_id();
+        assert!(i.is_ok());
+
+        let e = c.set_feed_path();
+
+        assert!(e.is_ok());
+        assert_eq!(c.feed_path, DATA_DIR.to_owned() + "UC9kZ6FlOQfusBV8LS2x2fAA");
+    }
+
+    #[test]
+    fn write_feed_to_file() {
+        let url = "https://www.youtube.com/channel/UC9kZ6FlOQfusBV8LS2x2fAA/";
+        let name = "Torsten Heinrich";
+        let mut c = channel::Channel::new(0, url.into(), name.into());
+        let feed = "This is some very interesting feed\n".to_owned();
+        c.feed = Some(feed);
+
+        let e = c.write_feed_to_file();
+
+        assert!(e.is_err());
+
+        let _ = c.set_id();
+        let _ = c.set_feed_path();
+
+        let w = c.write_feed_to_file();
+
+        assert!(w.is_ok());
+
+        let file_content = fs::read_to_string(c.feed_path).expect("Reading gone wrong");
+
+        assert_eq!(c.feed.unwrap(), file_content);
     }
 }
